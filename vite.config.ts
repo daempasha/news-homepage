19f58505385c083ee99813df26ed2d2e9449b69c
Daem Pasha <daempasha@gmail.com>
2023-01-08 19:48:03 +0000

    Added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,6 @@ const TestConfig: InlineConfig = {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  test: TestConfig
+  test: TestConfig,
+  base: "/news-homepage/"
 })
