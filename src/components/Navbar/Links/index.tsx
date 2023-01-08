135f8695739d58a691ea12a5fb06e678818540a7
Daem Pasha <daempasha@gmail.com>
2023-01-08 01:25:52 +0000

    Added anchor and formatting to links

--- src/components/Navbar/Links/index.tsx
+++ src/components/Navbar/Links/index.tsx
@@ -4,7 +4,7 @@ const Links = () => {
     return <div className="hidden md:block">
         <ul>
             {links.map((category) =>
-                <li className="inline mx-2 text-gray-600">{category}</li>
+                <li className="inline mx-2 text-gray-600"><a className="hover:text-orange-600 focus:text-orange-600 transition-colors delay-75" href="#">{category}</a></li>
             )}
 
         </ul>
