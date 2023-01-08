d2c538dee03850798fca1d02aae47cd80426c71a
Daem Pasha <daempasha@gmail.com>
2023-01-08 16:41:19 +0000

    Small Stories links are now working too

--- src/components/SmallStory/index.tsx
+++ src/components/SmallStory/index.tsx
@@ -9,7 +9,7 @@ const SmallStory: React.FC<SmallStoryProps> = ({ index, title, description, imag
             </div>
             <div className="col-span-7 md:col-span-7 lg:col-span-9">
                 <h1 className="text-3xl text-gray-300 font-bold ">{`${(index + 1)}`.padStart(2, "0")}</h1>
-                <h2 className="font-bold text-lg lg:text-xl my-2">{title}</h2>
+                <a className=" font-bold text-lg lg:text-xl my-2 cursor-pointer hover:text-orange-500">{title}</a>
                 <p className="text-sm lg:text-base text-gray-600">{description}</p>
             </div>
         </div>
